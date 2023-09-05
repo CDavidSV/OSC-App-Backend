bed1c5bd4fbb947e30209fc491883401d386f488
CDavidSV <88672259+CDavidSV@users.noreply.github.com>
2023-09-05 12:49:11 -0600

    Update auth.ts

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -2,7 +2,6 @@ import express from "express";
 import UserDB from "../scheemas/userSchema";
 import generateToken from "../util/generateJWT";
 import { User } from "../Models/interfaces";
-import generateAuthCode from "../util/generateCode";
 import RefreshToken from "../scheemas/refreshTokenScheema";
 import jwt from "jsonwebtoken";
 import admin from 'firebase-admin';
@@ -17,29 +16,54 @@ admin.initializeApp({
     }),
 });
 
-router.post('/login', async (req: express.Request, res: express.Response) => {
+const generateTokens = async (userId: string) => {
+    // Generate access token.
+    let expirationTime = 24 * 60 * 60; // 24 hours in seconds.
+    const accessToken = generateToken({ id: userId, refresh: false }, expirationTime);
+
+    // Generate refresh token.
+    expirationTime *= 7; // 7 days in seconds.
+    const refreshToken = generateToken({ id: userId, refresh: true }, expirationTime, true);
+    await RefreshToken.create({ userId: userId, token: refreshToken, expiresAt: new Date(Date.now() + expirationTime * 1000), createdAt: new Date() });
+
+    return { accessToken, refreshToken };
+}
+
+router.post('/register', async (req: express.Request, res: express.Response) => {
     const { accountToken, username } = req.body;
 
     if (!accountToken) return res.status(400).send({ status: "error", message: "No token specified" });
-    if (!username) return res.status(400).send({ status: "error", message: "No username provided" });
+    if (!username) return res.status(400).send({ status: "error", message: "No username specified" });
+
+    try {
+        const user = await admin.auth().verifyIdToken(accountToken);
+        await admin.auth().updateUser(user.uid, { displayName: username });
+        const dbUser = await UserDB.create({ _id: user.uid, username: username, email: user.email || null, phoneNumber: user.phone_number || null, profilePictureURL: user.picture || null });
+
+        const { accessToken, refreshToken } = await generateTokens(dbUser._id);
+        res.status(200).send({ status: "success", message: "User registered", accessToken: accessToken, refreshToken: refreshToken });
+    } catch (err) {
+        res.status(500).send({ status: "error", message: "Error registering user" });
+    }
+});
+
+router.post('/login', async (req: express.Request, res: express.Response) => {
+    const { accountToken } = req.body;
+
+    if (!accountToken) return res.status(400).send({ status: "error", message: "No token specified" });
     
     try {
         const user = await admin.auth().verifyIdToken(accountToken);
         let dbUser = await UserDB.findById({ _id: user.uid });
+
+        if (!user.displayName || !user.name) return res.status(401).send({ status: "error", message: "User not registered" });
     
         if (!dbUser) {
             // Create new user in database.
-            dbUser = await UserDB.create({ _id: user.uid, username, email: user.email || null, phoneNumber: user.phone_number || null, profilePictureURL: user.picture || null });
+            dbUser = await UserDB.create({ _id: user.uid, username: user.displayName || user.name, email: user.email || null, phoneNumber: user.phone_number || null, profilePictureURL: user.picture || null });
         };
-    
-        // Generate access token.
-        let expirationTime = 24 * 60 * 60; // 24 hours in seconds.
-        const accessToken = generateToken({ id: dbUser._id, refresh: false }, expirationTime);
-    
-        // Generate refresh token.
-        expirationTime *= 7; // 7 days in seconds.
-        const refreshToken = generateToken({ id: dbUser._id, refresh: true }, expirationTime, true);
-        await RefreshToken.create({ userId: dbUser._id, token: refreshToken, expiresAt: new Date(Date.now() + expirationTime * 1000), createdAt: new Date() });
+
+        const { accessToken, refreshToken } = await generateTokens(dbUser._id);
 
         res.status(200).send({ status: "success", message: "User login success", accessToken: accessToken, refreshToken: refreshToken });
     } catch (err) {
@@ -69,19 +93,13 @@ router.post('/refreshToken', async (req: express.Request, res: express.Response)
     if (!refreshToken) return res.status(400).send({ status: "error", message: "No token specified" });
 
     const tokenData: User = jwt.decode(refreshToken) as User;
-    if (!tokenData || !tokenData.refresh) return res.status(400).send({ status: "error", message: "Invalid token provided" });
+    if (!tokenData || !tokenData.id || !tokenData.refresh) return res.status(400).send({ status: "error", message: "Invalid token provided" });
 
     try {
         const deletedToken = await RefreshToken.findOneAndDelete({ token: refreshToken, userId: tokenData.id });
         if (!deletedToken) return res.status(400).send({ status: "error", message: "Invalid token provided" });
 
-        let expirationTime = 24 * 60 * 60;
-        const accessToken = generateToken({ id: tokenData.id, refresh: false }, expirationTime);
-
-        expirationTime *= 7; // 7 days in seconds.
-        const newRefreshToken = generateToken({ id: tokenData.id, refresh: true }, expirationTime, true);
-        await RefreshToken.create({ userId: tokenData.id, token: newRefreshToken, expiresAt: new Date(Date.now() + expirationTime * 1000), createdAt: new Date() });
-
+        const { accessToken, refreshToken: newRefreshToken } = await generateTokens(tokenData.id);
         res.status(200).send({ status: "success", message: "Token refreshed", newAccessToken: accessToken, refreshToken: newRefreshToken });
     } catch {
         res.status(500).send({ status: "error", message: "Error refreshing token. Please try again." });
