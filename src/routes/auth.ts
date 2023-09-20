779f6d7fabb32a8406f722126d0f4cab02f85dce
CDavidSV <88672259+CDavidSV@users.noreply.github.com>
2023-09-20 16:36:55 -0600

    Reimplemented refresh tokens

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import UserDB from "../scheemas/userSchema";
 import generateToken from "../util/generateJWT";
+import refreshTokenSchema from "../scheemas/refreshTokenSchema";
+import jwt from "jsonwebtoken";
 import { User } from "../Models/interfaces";
 import admin from "../config/initFirebase";
 
@@ -8,10 +10,14 @@ const router: express.Router = express.Router();
 
 const generateTokens = (userId: string) => {
     // Generate access token.
-    let expirationTime = 60 * 60 * 24 * 7; // 7 days in seconds.
+    let expirationTime = 24 * 60 * 60; // 24 hours in seconds.
     const accessToken = generateToken({ id: userId } as User, expirationTime);
 
-    return accessToken;
+    // Generate refresh token.
+    expirationTime *= 7; // 7 days in seconds.
+    const refreshToken = generateToken({ id: userId } as User, expirationTime, true);
+
+    return { accessToken, refreshToken };
 }
 
 router.post('/register', async (req: express.Request, res: express.Response) => {
@@ -39,8 +45,10 @@ router.post('/register', async (req: express.Request, res: express.Response) => 
         // Create the new user in the database
         const dbUser = await UserDB.create(filteredUserObject);
 
-        const accessToken = generateTokens(dbUser._id.toString());
-        res.status(200).send({ status: "success", message: "User registered", accessToken: accessToken });
+        const { accessToken, refreshToken } = generateTokens(dbUser._id.toString());
+        await refreshTokenSchema.create({ userId: dbUser._id, token: refreshToken, expiresAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), createdAt: new Date() });
+
+        res.status(200).send({ status: "success", message: "User registered", accessToken: accessToken, refreshToken: refreshToken });
     } catch (err) {
         res.status(500).send({ status: "error", message: "Error registering user" });
     }
@@ -84,12 +92,50 @@ router.post('/login', async (req: express.Request, res: express.Response) => {
             // Update new firebase id.
             await UserDB.findByIdAndUpdate(dbUser._id, { firebaseId: user.uid });
         }
-        const accessToken = generateTokens(dbUser._id.toString());
+        const { accessToken, refreshToken } = generateTokens(dbUser._id.toString());
+        await refreshTokenSchema.create({ userId: dbUser._id, token: refreshToken, expiresAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), createdAt: new Date() });
 
         res.status(200).send({ status: "success", message: "User login success", accessToken: accessToken });
     } catch {
         res.status(500).send({ status: "error", message: "Error logging in user"});
     }
+});
+
+router.post('/revoke', async (req: express.Request, res: express.Response) => {
+    const { refreshToken } = req.body;
+    if (!refreshToken) return res.status(400).send({ status: "error", message: "No token specified" });
+
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_KEY as string, (err: any, tokenData: any) => {
+        if (err) return res.sendStatus(401);
+
+        // Remove refresh token from database.
+        refreshTokenSchema.findOneAndDelete({ token: refreshToken, userId: tokenData.id }).then((deletedToken) => {  
+            if (!deletedToken) return res.status(400).send({ status: "error", message: "Invalid Token Provided" });  
+            res.status(200).send({ status: "success", message: "Token revoked" });
+        }).catch((err) => {
+            return res.status(500).send({status: "error", message: "Error revoking token" });
+        });
+    });
+});
+
+router.post('/refreshToken', async (req: express.Request, res: express.Response) => {
+    const { refreshToken } = req.body;
+    if (!refreshToken) return res.status(400).send({ status: "error", message: "No token specified" });
+
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_KEY as string, async (err: any, tokenData: any) => {
+        if (err) return res.sendStatus(401);
+
+        try {
+            const { accessToken, refreshToken: newRefreshToken } = generateTokens(tokenData.id);
+
+            // Update the current refresh token in the database.
+            const updatedToken = await refreshTokenSchema.findOneAndUpdate({ token: refreshToken, userId: tokenData.id }, { token: newRefreshToken, expiresAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000) });
+            if (!updatedToken) return res.status(400).send({ status: "error", message: "Invalid Token Provided" });
+            res.status(200).send({ status: "success", message: "Token refreshed", newAccessToken: accessToken, refreshToken: newRefreshToken });
+        } catch {
+            res.status(500).send({ status: "error", message: "Error refreshing token. Please try again." });
+        }
+    });
 });
 
 export default router;
