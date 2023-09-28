4cbe33c95d5936c3bcfc0fe85afa8e9da8b0d4aa
CDavidSV <88672259+CDavidSV@users.noreply.github.com>
2023-09-28 17:36:36 -0600

    Update associationSchema.ts

--- src/scheemas/associationSchema.ts
+++ src/scheemas/associationSchema.ts
@@ -1,17 +1,23 @@
-import { Schema, model } from "mongoose";
+import mongoose, { Schema, model } from "mongoose";
+
+const colaboratorSchema = new mongoose.Schema({
+    userId: { type: String, required: true },
+    perms: { type: Number, required: true },
+});
 
 const AssociationSchema = new Schema({
     name: { type: String, required: true },
     description: { type: String, default: null },
+    ownerId: { type: String, required: true },
+    colaborators: { type: [colaboratorSchema], default: [] },
     logoURL: { type: String, default: null },
     images: { type: [String], default: [] },
-    thumbnailURL: { type: String, default: null },
     websiteURL: { type: String, default: null },
     facebookURL: { type: String, default: null },
     instagramURL: { type: String, default: null },
     categoryId: { type: Schema.Types.ObjectId, required: true },
     tags: { type: [String], default: [] },
-    contact : {
+    contact: {
         email: { type: String, required: true },
         phone: { type: String, required: true },
         whatsapp: { type: String, default: null },
@@ -20,6 +26,7 @@ const AssociationSchema = new Schema({
     verified: { type: Boolean, required: true }
 });
 
-AssociationSchema.index({ name: 1});
-AssociationSchema.index({ category: 1});
+AssociationSchema.index({ name: 1 });
+AssociationSchema.index({ category: 1 });
+AssociationSchema.index({ tags: 1 });
 export default model("Association", AssociationSchema);
