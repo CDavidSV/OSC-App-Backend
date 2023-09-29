9ae8fba3c76bdabe57fc05fba718dc68727d283b
raxhacks <alexhacker654@gmail.com>
2023-09-28 18:05:08 -0600

    middlewares

---
+++ src/middlewares/verify-user-owner.ts
@@ -0,0 +1,28 @@
+import { Request, Response, NextFunction } from 'express';
+import UserDB from "../scheemas/userSchema";
+
+async function verifyUserIsAdmin(req: Request, res: Response, next: NextFunction) {
+    const { user_id } = req.body || req.query;
+    if (!user_id) {
+        return res.status(400).json({ message: 'Missing user_id in request' });
+    }
+    
+    UserDB.findOne({ _id: user_id })
+        .then(user => {
+            if (!user) {
+                return res.status(404).json({ message: 'User not found' });
+            }
+            if (user.assocPerms === 1) {
+                return next();
+            } else {
+                return res.status(401).json({ message: 'Unauthorized' });
+            }
+        })
+        .catch(e => {
+            console.log("Error:",e);
+            return res.status(500).json({message: 'Internal Server Error. Contact support.'})
+        });
+}
+
+
+export default verifyUserIsAdmin;
