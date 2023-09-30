a4d261bbbd294bfa549114f7f67a4fca1a82365d
CDavidSV <88672259+CDavidSV@users.noreply.github.com>
2023-09-30 15:02:15 -0600

    Update validateInputSchema.ts

--- src/util/validateInputSchema.ts
+++ src/util/validateInputSchema.ts
@@ -10,28 +10,16 @@ interface JsonValidatorResponse {
 }
 
 const validateJsonBody = (body: any, schema: { [key: string]: JsonValidator }) => {
-    const missing = [];
-    const invalid = [];
-    for (const key in schema) {
-        if (!schema.hasOwnProperty(key)) continue;
-        const value = body[key];
-        const validator = schema[key];
-
-        if ((value === null || value === undefined) && validator.required) {
-            missing.push(key);
-            continue;
-        } else if (value === null || value === undefined) {
-            continue;
-        }
-
-        if (validator.type === 'array' && !Array.isArray(value)) {
-            invalid.push(key);
-            continue;
-        } else if (typeof value !== validator.type) {
-            invalid.push(key);
-            continue;
-        }
-    }
+    let missing: string[] = [];
+    let invalid: string[] = [];
+    const keys = Object.keys(schema);
+    missing = keys.filter((key) => schema[key].required && (body[key] === null || body[key] === undefined));
+    invalid = keys.filter((key) => {
+        const value = body[key] ?? null;
+        if (value === null) return false;
+    
+        return schema[key].type === 'array' ? !Array.isArray(value) : typeof value !== schema[key].type;
+    });
 
     return {
         valid: missing.length === 0 && invalid.length === 0,
